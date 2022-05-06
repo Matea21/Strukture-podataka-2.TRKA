d2d9d1196c054d36d9d44a19012fdc26baa00984
Matea21 <73309462+Matea21@users.noreply.github.com>
2022-05-06 10:47:40 +0200

    Update zad3.c

--- zad3.c
+++ zad3.c
@@ -3,61 +3,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 1024
 
-/*3. Prethodnom zadatku dodati funkcije:
-a) dinamički dodaje novi element iza određenog elementa,
-b) dinamički dodaje novi element ispred određenog elementa,
-c) sortira listu po prezimenima osoba,
-d) upisuje listu u datoteku,
-e) čita listu iz datoteke.*/
+
+#define MAX 1024
 
 typedef struct _osoba {
 	char ime[MAX];
 	char prezime[MAX];
 	int godinaRodenja;
-	struct _osoba* next;
-} Osoba;
+	struct _osoba*next;
+}Osoba;
 
+int Menu(Osoba*Head);
 Osoba* UnosPodataka(void);
-int DodatiElementNaPocetak(Osoba* El, Osoba* Head);
-int IspisElemenata(Osoba* Head);
-int DodatiElementNaKraj(Osoba* El, Osoba* Head);
-Osoba* PronadiElementPoPrezimenu(Osoba* Head, char* prezime);
-int IzbrisiElement(Osoba* El, Osoba* Head);
-int DodajElementIza(Osoba* noviEl, Osoba* El);
-int DodajElementIspred(Osoba* Head, Osoba* El);
-int SortirajListu(Osoba* Head);
-int ZamijeniPozicije(Osoba* Head, Osoba* El1, Osoba* El2);
-Osoba* PronadiPrethodniElement(Osoba* Head, Osoba* El);
-Osoba* PronadiZadnjiElement(Osoba* Head);
-int SpremiListuUDatoteku(Osoba* Head, char* datoteka);
-int UcitajListuIzDatoteke(Osoba* Head, char* datoteka);
+int DodatiElementNaPocetak(Osoba *El, Osoba*Head);
+int IspisElemenata(Osoba*Head);
+int DosatiElementNaKraj(Osoba*El, Osoba*Head);
+Osoba* PronadiElementPoPrezimenu(Osoba*Head, char*prezime);
+int IzbrisiElement(Osoba*El, Osoba*Head);
+int DodajElementIza(Osoba* noviEl, Osoba*El);
+int DodajElementIspred(Osoba*Head, Osoba*El);
+Osoba*PronadiPrethodniElement(Osoba*Head, Osoba*El);
+Osoba* PronadiZadnjiElement(Osoba*Head);
+int SpremiListuUDatoteku(Osoba*Head, char*datoteka);
+int UcitajListuIzDatoteke(Osoba*Head, char*datoteka);
 
-int main(void)
-{
-	Osoba* Head = (Osoba*)malloc(sizeof(Osoba));
+int main() {
+
+	Osoba*Head = (Osoba*)malloc(sizeof(Osoba));
 	Head->next = NULL;
+	int status = 0;
+	Osoba*El = (Osoba*)malloc(sizeof(Osoba));
 
-	int a = 0;
+	do {
+		status =Menu(Head);
+	} while (!status);
+
+	return 0;
+}
+int Menu(Osoba*Head) {
+	int c = 0;
+	int status = 0;
 	char prezime[MAX];
 	char datoteka[MAX];
-	Osoba* El = (Osoba*)malloc(sizeof(Osoba));
+	Osoba*El = (Osoba*)malloc(sizeof(Osoba));
 
 	printf("Odaberite zeljenu opciju: \n");
-	printf("1. Dodati novi element na pocetak liste\n");
-	printf("2. Ispisati listu\n");
-	printf("3. Dodati novi element na kraj liste\n");
-	printf("4. Pronaci element u listi (po prezimenu)\n");
-	printf("5. Brisati odredeni element\n");
-	printf("6. Dodati element iza nekog elementa\n");
-	printf("7. Dodati element ispred nekog elementa\n");
-	printf("8. Sortirati listu\n");
-	printf("9. Spremiti listu u datoteku\n");
-	printf("10. Ucitati listu iz datoteke\n");
-	scanf("%d", &a);
+	printf("1.Dodati element na početak liste\n");
+	printf("2.Ispisati listu\n");
+	printf("3.Dodati element na kraj liste\n");
+	printf("4.Pronadi osobu po prezimenu\n");
+	printf("5.Izbrisi element\n");
+	printf("6.Dodaj element iza nekog elementa\n");
+	printf("7.Dodaj element ispred nekog elementa\n");
+	printf("8.Spremi listu u datoteku\n");
+	printf("9.Ucitaj listu iz datoteke\n");
+	scanf("%d", &c);
 
-	switch (a) {
+	do {
+		status = c;
+	} while (!status);
+
+	switch (c) {
 	case(1):
 		DodatiElementNaPocetak(UnosPodataka(), Head);
 		break;
@@ -65,76 +72,78 @@ int main(void)
 		IspisElemenata(Head);
 		break;
 	case(3):
-		DodatiElementNaKraj(UnosPodataka(), Head);
+		DosatiElementNaKraj(UnosPodataka(), Head);
 		break;
 	case(4):
-		printf("Upisite prezime elementa kojeg zelite pronaci: ");
+		printf("Upisite prezime elementa kojeg zelite pronaci:");
 		scanf("%s", prezime);
 		El = PronadiElementPoPrezimenu(Head, prezime);
+		printf("%s	%s	%d", El->ime, El->prezime, El->godinaRodenja);
 		break;
 	case(5):
-		printf("Upisite prezime elementa kojeg zelite izbrisati: ");
+		printf("Upisite prezime elementa kojeg zelite izbrisati:");
 		scanf("%s", prezime);
 		IzbrisiElement(PronadiElementPoPrezimenu(Head, prezime), Head);
 		break;
 	case(6):
-		printf("Upisite prezime elementa iza kojeg zelite dodati novi element: ");
+		printf("Upisite prezime elementa iza kojeg zelite dodati novi element:");
 		scanf("%s", prezime);
-		DodajElementIza(UnosPodataka(), PronadiElementPoPrezimenu(Head, prezime));
+		El = PronadiElementPoPrezimenu(Head, prezime);
+		DodajElementIza(UnosPodataka(), El);
 		break;
 	case(7):
-		printf("Upisite prezime elementa ispred kojeg zelite dodati novi element: ");
+		printf("Upisite prezime elementa ispred kojeg zelite dodati novi element:");
 		scanf("%s", prezime);
-		DodajElementIspred(Head, PronadiElementPoPrezimenu(Head, prezime));
+		DodajElementIspred(Head,PronadiElementPoPrezimenu(Head,prezime));
 		break;
 	case(8):
-		SortirajListu(Head);
-		break;
-	case(9):
-		printf("Upisite ime datoteke: ");
+		printf("Upisite ime daoteke:");
 		scanf("%s", datoteka);
 		SpremiListuUDatoteku(Head, datoteka);
 		break;
-	case(10):
-		printf("Upisite ime datoteke: ");
+	case(9):
+		printf("Upisite ime datoteke:");
 		scanf("%s", datoteka);
 		UcitajListuIzDatoteke(Head, datoteka);
 		break;
 	default:
 		printf("Krivi unos");
+
+	case (0):
+		return 1;
 	}
-
-	return 0;
 }
-Osoba* UnosPodataka(void) {
-	Osoba* El = (Osoba*)malloc(sizeof(Osoba));
 
-	printf("Unesite ime, prezime i godinu rodenja: \n");
+Osoba* UnosPodataka(void) {
+	Osoba*El = (Osoba*)malloc(sizeof(Osoba*));
+
+	printf("Unesite ime,prezime i godinu rodenja:\n");
 	scanf("%s %s %d", El->ime, El->prezime, &(El->godinaRodenja));
 
 	return El;
 }
-int DodatiElementNaPocetak(Osoba* El, Osoba* Head) {
 
+int DodatiElementNaPocetak(Osoba *El, Osoba*Head) {
 	El->next = Head->next;
 	Head->next = El;
 
 	return 0;
 }
-int IspisElemenata(Osoba* Head) {
-	Osoba* P = Head->next;
+int IspisElemenata(Osoba*Head) {
+	Osoba*P = Head->next;
 
 	while (P != NULL) {
-		printf("%s %s %d", P->ime, P->prezime, P->godinaRodenja);
-		P = P->next;
+		printf("Ime:%s	Prezime:%s	Godina rodenja:%d\t\n", P->ime, P->prezime, P->godinaRodenja);
+		P=P->next;
 	}
 
 	return 0;
 }
-int DodatiElementNaKraj(Osoba* El, Osoba* Head) {
-	Osoba* P = Head->next;
+int DosatiElementNaKraj(Osoba*El, Osoba*Head) {
 
-	while (P != NULL)
+	Osoba *P = Head;
+
+	while (P->next != NULL)
 		P = P->next;
 
 	P->next = El;
@@ -142,36 +151,42 @@ int DodatiElementNaKraj(Osoba* El, Osoba* Head) {
 
 	return 0;
 }
-Osoba* PronadiElementPoPrezimenu(Osoba* Head, char* prezime) {
-	Osoba* P = Head->next;
+Osoba* PronadiElementPoPrezimenu(Osoba*Head, char*prezime) {
 
-	while (P != NULL && P->prezime != prezime)
+	Osoba*P = Head->next;
+
+	while (P != NULL && strcmp(P->prezime,prezime))
 		P = P->next;
 
 	return P;
 }
-int IzbrisiElement(Osoba* El, Osoba* Head) {
-	Osoba* P = Head->next;
+int IzbrisiElement(Osoba*El, Osoba*Head) {
+	Osoba*P = Head;
 
-	while (P != NULL && P->next != El)
+	while (P!= NULL && P->next!= El)
 		P = P->next;
 
-	P->next = El->next;
-	free(El);
+	if (P == NULL)
+		return -1;
+	else
+	{
+		P->next = El->next;
+		free(El);
+	}
 
 	return 0;
 }
-int DodajElementIza(Osoba* noviEl, Osoba* El) {
+int DodajElementIza(Osoba* noviEl, Osoba*El) {
 
 	noviEl->next = El->next;
 	El->next = noviEl;
 
 	return 0;
 }
-int DodajElementIspred(Osoba* Head, Osoba* El) {
+int DodajElementIspred(Osoba*Head, Osoba*El) {
 
-	Osoba* noviEl = (Osoba*)malloc(sizeof(Osoba));
-	Osoba* P = Head;
+	Osoba*noviEl = (Osoba*)malloc(sizeof(Osoba));
+	Osoba*P = Head;
 
 	while (P != NULL && P->next != El)
 		P = P->next;
@@ -180,55 +195,30 @@ int DodajElementIspred(Osoba* Head, Osoba* El) {
 
 	return 0;
 }
-int SortirajListu(Osoba* Head) {
-	Osoba* X = Head->next;
-	Osoba* Y = NULL;
-
-	while (Head->next != Y) {
-		while (X->next != Y) {
-			if (strcmp(X->prezime, X->next->prezime) > 0) {
-				ZamijeniPozicije(Head, X, X->next);
-			}
-			else
-				X = X->next;
-		}
-	}
-	Y = PronadiPrethodniElement(Head, Y);
-	X = Head->next;
-
-	return 0;
-}
-int ZamijeniPozicije(Osoba* Head, Osoba* El1, Osoba* El2) {
-	Osoba* prethodniEl = PronadiPrethodniElement(Head, El1);
-	Osoba* temp = El2->next;;
-
-	prethodniEl->next = El2;
-	El2->next = El1;
-	El1->next = temp;
-
-	return 0;
-}
-Osoba* PronadiPrethodniElement(Osoba* Head, Osoba* El) {
-	Osoba* P = Head;
+Osoba*PronadiPrethodniElement(Osoba*Head, Osoba*El) {
+	Osoba*P = Head;
 
 	while (P != NULL && P->next != El)
 		P = P->next;
 
 	return P;
 }
-Osoba* PronadiZadnjiElement(Osoba* Head) {
+Osoba* PronadiZadnjiElement(Osoba*Head) {
+
 	return PronadiPrethodniElement(Head, NULL);
 }
-int SpremiListuUDatoteku(Osoba* Head, char* datoteka) {
+int SpremiListuUDatoteku(Osoba*Head, char*datoteka) {
 	FILE* fp = NULL;
-	Osoba* P = Head->next;
+	Osoba*P = Head->next;
 
 	fp = fopen(datoteka, "w");
 	if (fp == NULL)
 		return -1;
 
+	fprintf(fp, "IME		PREZIME		GODINA ROĐENJA\n");
+
 	while (P != NULL) {
-		printf(fp, "%s %s %d", P->ime, P->prezime, P->godinaRodenja);
+		fprintf(fp, "%s		%s		%d\n", P->ime, P->prezime, P->godinaRodenja);
 		P = P->next;
 	}
 
@@ -236,27 +226,36 @@ int SpremiListuUDatoteku(Osoba* Head, char* datoteka) {
 
 	return 0;
 }
-int UcitajListuIzDatoteke(Osoba* Head, char* datoteka) {
-	FILE* fp = NULL;
+int UcitajListuIzDatoteke(Osoba*Head, char*datoteka) {
+	FILE*fp = NULL;
 	int brojOsoba = 0;
-	char buffer[MAX];
-	Osoba* noviEl = (Osoba*)malloc(sizeof(Osoba));
-	Osoba* P = PronadiZadnjiElement(Head);
+	char buffer[MAX] = { 0 };
+	Osoba*noviEl = (Osoba*)malloc(sizeof(Osoba));
+	Osoba*P = PronadiZadnjiElement(Head);
+
+	fp = fopen(datoteka, "r");
+
+	if (fp == NULL)
+		return-1;
+
+	while (getc(fp) != '\n')
+		continue;
 
 	while (fgets(buffer, MAX, fp))
 		brojOsoba++;
+	rewind(fp);
 
-	fp = fopen(datoteka, "r");
-	if (fp == NULL)
-		return -1;
+	while (getc(fp) != '\n')
+		continue;
 
 	for (int i = 0; i < brojOsoba; i++) {
-		P->next = noviEl;
 		noviEl->next = NULL;
+		P->next = noviEl;
 		P = P->next;
 
-		scanf(fp, "%s %s %d", noviEl->ime, noviEl->prezime, &(noviEl->godinaRodenja));
+		fscanf(fp, "%s %s %d", noviEl->ime, noviEl->prezime, &(noviEl->godinaRodenja));
 	}
+
 	fclose(fp);
 
 	return 0;
