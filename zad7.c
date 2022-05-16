f5b12cfef17d23a38c2c8a4edd443089c1098a57
Matea21 <73309462+Matea21@users.noreply.github.com>
2022-05-16 19:21:54 +0200

    Update zad7.c

--- zad7.c
+++ zad7.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*8. Napisati program koji pomoću vezanih listi (stabala) predstavlja strukturu direktorija.
+/*7. Napisati program koji pomoću vezanih listi (stabala) predstavlja strukturu direktorija.
 Omogućiti unos novih direktorija i pod-direktorija, ispis sadržaja direktorija i
 povratak u prethodni direktorij. Točnije program treba preko menija simulirati
 korištenje DOS naredbi: 1- "md", 2 - "cd dir", 3 - "cd..", 4 - "dir" i 5 – izlaz.*/
